c45a54035aad97966a9fb0c01848554721e70d71
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-05-09 22:28:16 +0200

    minor change to commit in mongodb branch and push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('ENTRANDO');
+  console.log('ENTRANDO EN MONGODB');
   res.status(200).json({status: 'OK'}); 
 });
 
